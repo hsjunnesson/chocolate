a5e35c7c725109b7bbc5dbf07a21f138b92fb2c9
Hans Sjunnesson <hans.sjunnesson@fatshark.se>
2022-07-23 23:10:53 +0200

    OpenGL 4.1 using GL_ARB_buffer_storage and GL_KHR_debug

--- src/engine.cpp
+++ src/engine.cpp
@@ -242,7 +242,7 @@ Engine::Engine(Allocator &allocator, const char *config_path)
         }
 
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
         glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
